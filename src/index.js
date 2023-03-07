11184a5de2647f0762e21e94f3a1f792521217bf
okewunmi <okewunmiafeezolaide@gmail.com>
2023-03-07 14:23:23 +0100

    another commit state

--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
+
+//dev-6cc3rl0lm72k1tj4.us.auth0.com
+//OHwRpnHyIUq6MEYHcovneNztbjaypcES
+
+//dev-ifivcyr2mexjqowy.us.auth0.com
+//LMAFQc6oGCYZVwPxb3we8ui2R8Aq21h9
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-ifivcyr2mexjqowy.us.auth0.com"
+      clientId="LMAFQc6oGCYZVwPxb3we8ui2R8Aq21h9"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        cacheLocation: "localstorage",
+      }}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
